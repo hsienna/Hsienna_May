0fd1bfd31ba7a5281c7133425178e29328e0dab7
Hsienna May Luy <luyhsiennamay.cpsu@gmail.com>
2026-09-23 18:34:58 +0800

    Update student page

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 $conn = new mysqli("127.0.0.1", "root", "", "student_db");
 
 if ($conn->connect_error) {
@@ -6,6 +7,7 @@ if ($conn->connect_error) {
 }
 
 if (isset($_POST['submit'])) {
+
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
 
@@ -14,19 +16,56 @@ if (isset($_POST['submit'])) {
 
     $conn->query($sql);
 }
+
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
+
     <title>My First PHP Page</title>
+
+    <style>
+
+        body {
+            font-size: 18px;
+        }
+
+        h1 {
+            font-size: 32px;
+        }
+
+        input {
+            font-size: 18px;
+            padding: 8px;
+        }
+
+        button {
+            font-size: 18px;
+            padding: 9px 20px;
+            background-color: blue;
+            color: white;
+        }
+
+        table {
+            font-size: 18px;
+        }
+
+        th, td {
+            padding: 12px;
+        }
+
+    </style>
+
 </head>
 
 <body>
 
-    <h1>Student List</h1>
+    <h1>My First PHP Page</h1>
 
     <form method="POST">
+
         <label>First Name:</label>
         <input type="text" name="first_name" required>
 
@@ -38,11 +77,13 @@ if (isset($_POST['submit'])) {
         <br><br>
 
         <button type="submit" name="submit">Submit</button>
+
     </form>
 
     <br>
 
-    <table border="1" cellpadding="10">
+    <table border="1">
+
         <tr>
             <th>ID</th>
             <th>First Name</th>
@@ -51,10 +92,36 @@ if (isset($_POST['submit'])) {
         </tr>
 
         <?php
+
         $result = $conn->query("SELECT * FROM students");
 
         while ($row = $result->fetch_assoc()) {
+
             echo "<tr>";
+
             echo "<td>" . $row['id'] . "</td>";
+
             echo "<td>" . $row['first_name'] . "</td>";
-            echo "<
+
+            echo "<td>" . $row['last_name'] . "</td>";
+
+            echo "<td>";
+
+            echo "<a href='edit.php?id=" . $row['id'] . "'>Edit</a>";
+
+            echo " | ";
+
+            echo "<a href='delete.php?id=" . $row['id'] . "'>Delete</a>";
+
+            echo "</td>";
+
+            echo "</tr>";
+        }
+
+        ?>
+
+    </table>
+
+</body>
+
+</html>
